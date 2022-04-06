68f815dc8b0fe56e123ce1235fcad35c89a87213
Your Name <you@example.com>
2022-04-06 00:47:32 -0700

    added visualizations

--- src/line chart/linechart.js
+++ src/line chart/linechart.js
@@ -1,48 +1,87 @@
-function main(){
+function main() {
 
-    var svg = d3.select('svg'),
-        margin = {
-            top: 20,
-            right:80,
-            bottom: 30,
-            left:50,
-        }, 
-        width = svg.attr("width")-margin.left - margin.right,
-        height = svg.attr("height")-margin.top - margin.bottom,
-        g = svg.append("g").attr("transform","translate("+margin.left +","+margin.top+")");
+    const canvasWidth = 800;
+    const canvasHeight = 800;
+    const margin = 400;
 
-        const parseTime = d3.timeParse("%y-%b")
 
-    var x = d3.scaleTime().range([0,width - margin.right]),
-        y = d3.scaleLinear().range([height,0]),
-        z = d3.scaleOrdinal(d3.schemeCategory10);
+    const svg = d3.select("body").append("svg")
+        .attr("width", canvasWidth)
+        .attr("height", canvasHeight)
 
-    var line = d3.line().
-        curve(d3.curveBasis)
-        .x(function(d){return x(d.Month); })
-        .y(function(d){ return y(d.Prices);});
+    const width = svg.attr("width") - margin;
+    const height = svg.attr("height") - margin;
+    
+    const parseTime = d3.timeParse("%y-%b")
 
-    d3.csv("../../CSV/condensed_ebay_prices_etherium .csv",function(d){
+    const container_g = svg.append("g")
+        .attr("transform","translate(100,100)");
+
+    d3.csv("../../CSV/condensed_ebay_prices_etherium .csv", function (d) {
         return d;
-    }).then(function(data){
+    }).then(function (data) {
 
-        let categories = Array.from(new Set(data.map(d=>d.Item)));
-
-        for (d of data){
+        for (d of data) {
             d.date = parseTime(d.Month);
-            console.log(d.date)
-            for(var i =0,n=categories.length, c; i <n; ++i) d[c = categories[i]] = +d[c];
-            
+            d.prices = +d.Prices;
         }
 
-        const items = data.categories.map(function(id){
-            return{
-                id: id,
-                values: data.map(function(d){
-                    return {date:d.date,prices:d[id]}
+        const xScale = d3.scaleTime()
+            .domain(d3.extent(data, (d) => { return d.date }))
+            .range([0, width]);
+
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(data, d => Number(d.Prices.replace(/[^0-9.-]+/g, "")))])
+            .range(([height, 0]));
+
+        const sumstat = d3.group(data, d => d.Item)
+        container_g.append("g")
+            .attr("transform","translate(0,"+ height + ")")
+            .call(d3.axisBottom(xScale))
+            .append("text")
+            .attr("y", height - 350)
+            .attr("x",width - 250)
+            .attr("font-size","30px")
+            .attr("stroke","black")
+            .text("Months")
+
+        container_g.append("g")
+            .call(d3.axisLeft(yScale))
+            .append("text")
+            .attr("font-size","25px")
+            .attr("transform","rotate(-90)")
+            .attr("y",40)
+            .attr("x",-80)
+            .attr("dy","-4.1em")
+            .attr("stroke","black")
+            .text("Prices for Items")
+
+             //color scale for the different plays that are being represented.
+        const color = d3.scaleOrdinal()
+            .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#1e90ff','seagreen','aquamarine','yellow','blue','pink','coral','cyan'])
+
+         container_g.selectAll(".line")
+            .data(sumstat)
+            .join("path")
+            .attr("fill","none")
+            .attr("stroke",(d)=>{ return color(d[0])})
+            .attr("stroke-width",1.5)
+            .attr("d",function(d){
+                return d3.line()
+                    .x(d => xScale(d.date))
+                    .y(d => yScale(d.prices))(d[1])
                 })
-            }
-        })
+
+        container_g.selectAll(".label")
+                .data(sumstat)
+                .enter()
+                .append("text")
+                .datum(function(d){ return{id:d.date,value:d.prices}})
+                .attr("class","label")
+                .attr("transform", function(d){ return "translate(" + xScale(d.value.date)+ ","+ yScale(d.value.prices)+ ")";})
+                .attr("x", 55)
+                .attr("y",15)
+
     });
 }
 main();
--- src/violin plot/violinplot.js
+++ src/violin plot/violinplot.js
@@ -1,75 +1,86 @@
 function main() {
-    const canvasWidth = 800;
-    const canvasHeight = 800;
-    const margin = 200;
+    // set the dimensions and margins of the graph
+    var margin = { top: 10, right: 30, bottom: 30, left: 40 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
 
-    const svg = d3.select("body").append("svg")
-        .attr("width",canvasWidth)
-        .attr("height",canvasHeight)
+    // append the svg object to the body of the page
+    var svg = d3.select("#my_dataviz")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-    const width = svg.attr("width") - margin;
-    const height = svg.attr("height") - margin;
+    // Read the data and compute summary statistics for each specie
+    d3.csv("../../CSV/stock_and_time-(Nvidia).csv", function (data) {
 
-    svg.append("text")
-        .attr("x",50)
-        .attr("y",50)
-        .attr("font-size","20px")
-        .attr("font-family","sans-serif")
-        .text("Violin Plot for Distribution of In Stock Times for Nvidia GPUs")
+        // Build and Show the Y scale
+        var y = d3.scaleLinear()
+            .domain([0, 60]) // Note that here the Y scale is set manually
+            .range([height, 0])
+        svg.append("g").call(d3.axisLeft(y).ticks(10))
 
-    const xScale = d3.scaleBand().range([0,width]).padding(0.3);
-    const yScale = d3.scaleLinear().range([height,0]);
+        // Build and Show the X scale. It is a band scale like for a boxplot: each group has an dedicated RANGE on the axis. This range has a length of x.bandwidth
+        var x = d3.scaleBand()
+            .range([0, width])
+            .domain(data.map((d) => {
+                return d['chipset'];
+            }))
+            .padding(0.05)     // This is important: it is the space between 2 groups. 0 means no padding. 1 is the maximum.
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
 
-    const container_g = svg.append("g")
-        .attr("transform","translate(100,100)");
-
-    d3.csv("../../CSV/stock_and_time - (Nvidia).csv",(d)=> {return{ model:d.chipset,stock:+d["time(minutes)"]}}).then((data)=>{
-        xScale.domain(data.map((d)=>{
-            return d['model'];
-        }))
-        yScale.domain([0,d3.max(data,(d)=>{return d['stock']})])
-        container_g.append("g")
-        .attr("transform","translate(0,"+height+")")
-        .call(d3.axisBottom(xScale))
-
-
-        container_g.append("g")
-            .call(d3.axisLeft(yScale))
-
+        // Features of the histogram
         var histogram = d3.histogram()
-            .domain(yScale.domain())
-            .thresholds(yScale.ticks(20))
-            .value(d=>d)
+            .domain(y.domain())
+            .thresholds(y.ticks(10))    // Important: how many bins approx are going to be made? It is the 'resolution' of the violin plot
+            .value(d => d)
 
-        var sumstat = d3.rollup(data,
-            a=>{
-            let input = a.map(g=>g["stock"]);
-            return histogram(input);
-            },
-            b=>b["model"]
-        );
-
-         const maxNum = Array.from(sumstat.values())
-            .map((value, key) => d3.max(value.map(d => d.length)))
-            .reduce((a, b) => a > b? a: b);
-
-        var xNum = d3.scaleLinear()
-            .range([0,xScale.bandwidth()])
-            .domain([0,maxNum])
-
-        container_g.selectAll("myViolin")
-            .data(sumstat)
-            .enter()
-            .append("g")
-            .attr("transform", function(d){
-                return("translate("+(xScale(d[0]))+","+0+")")
+        // Compute the binning for each group of the dataset
+        var sumstat = d3.nest()  // nest function allows to group the calculation per level of a factor
+            .key(function (d) { return d.chipset; })
+            .rollup(function (d) {   // For each key..
+                let input = d.map(function (g) { return g['time(minutes)']; })    // Keep the variable called Sepal_Length   // And compute the binning on it.
+                return (histogram(input))
             })
+            .entries(data)
 
-        
-    
-    });
+        // What is the biggest number of value in a bin? We need it cause this value will have a width of 100% of the bandwidth.
+        var maxNum = 0
+        for (i in sumstat) {
+            allBins = sumstat[i].value
+            lengths = allBins.map(function (a) { return a.length; })
+            longuest = d3.max(lengths)
+            if (longuest > maxNum) { maxNum = longuest }
+            console.log(i);
+        }
 
-    // const
+        // The maximum width of a violin must be x.bandwidth = the width dedicated to a group
+        var xNum = d3.scaleLinear()
+            .range([0, x.bandwidth()])
+            .domain([-maxNum, maxNum])
+
+        // Add the shape to this svg!
+        svg
+            .selectAll("myViolin")
+            .data(sumstat)
+            .enter()        // So now we are working group per group
+            .append("g")
+            .attr("transform", function (d) { return ("translate(" + x(d.key) + " ,0)") }) // Translation on the right to be at the group position
+            .append("path")
+            .datum(function (d) { return (d.value) })     // So now we are working bin per bin
+            .style("stroke", "none")
+            .style("fill", "#69b3a2")
+            .attr("d", d3.area()
+                .x0(function (d) { return (xNum(-d.length)) })
+                .x1(function (d) { return (xNum(d.length)) })
+                .y(function (d) { return (y(d.x0)) })
+                .curve(d3.curveCatmullRom)    // This makes the line smoother to give the violin appearance. Try d3.curveStep to see the difference
+            )
+    })
 }
 
 main();
