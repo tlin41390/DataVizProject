9d451660af85dfa8aa147e308f35ac827978d47d
tlin41390 <tlin41@dons.usfca.edu>
2022-04-13 19:38:27 -0700

    added more histograms.

--- src/histogram/histogram.js
+++ src/histogram/histogram.js
@@ -11,106 +11,75 @@ function main() {
     const width = svg.attr("width") - margin;
     const height = svg.attr("height") - margin;
 
-    //add text to the canvas for a title
-    svg.append("text")
-        .attr("transform", "translate(100, 0)")
-        .attr("x", 100)
-        .attr("y", 100)
-        .attr("font-size", "20px")
-        .attr("font-family", "sans-serif")
-        .text("Histogram of Sell Out Times For GPUs")
-
     const xScale = d3.scaleLinear().range([0, width / 2]);
     const yScale = d3.scaleLinear().range([height / 2, 0]);
 
     const x = d3.scaleLinear().range([width / 2 + 30, width])
     const y = d3.scaleLinear().range([height / 2, 0])
 
+    const thirdX = d3.scaleLinear().range([0, width / 2])
+    const thirdY = d3.scaleLinear().range([width, height / 2 + 30])
+
     const container_g = svg.append("g")
         .attr("transform", "translate(100,100)");
 
     d3.csv("../../CSV/stock_and_time-(Nvidia).csv").then(data => {
         xScale.domain([0, 60]);
-        yScale.domain([0, 200]);
+        yScale.domain([0, 400]);
         x.domain([0, 60]);
-        y.domain([0, 200]);
+        y.domain([0, 400]);
+        thirdX.domain([0, 60]);
+        thirdY.domain([0, 400]);
+
         let distributions_thirtyseventy = []
         let distributions_thirtyeighty = []
+        let distributions_thirtyseventy_ti = []
         data.forEach((d) => {
             if (d.chipset == "rtx-3070") {
                 distributions_thirtyseventy.push(d["time(minutes)"])
             } else if (d.chipset == "rtx-3080") {
                 distributions_thirtyeighty.push(d["time(minutes)"])
+            } else if (d.chipset == "rtx-3080ti") {
+                distributions_thirtyseventy_ti.push(d["time(minutes)"])
             }
         })
-        console.log(distributions_thirtyeighty)
-
         container_g.append("g")
             .attr("transform", "translate(0, " + height / 2 + ")")
             .call(d3.axisBottom(x))
-            .append("text")
-            .attr("y", height - 450)
-            .attr("x", width -100)
-            .attr("stroke", "black")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "20px")
-            .text("Time To Sell Out(Minutes)");
+
+
+        container_g.append("g")
+            .attr("transform", "translate(0, " + height + ")")
+            .call(d3.axisBottom(thirdX))
+
+        container_g.append("g")
+            .call(d3.axisLeft(thirdY).tickFormat(function (d) {
+                return d;
+            }).ticks())
 
         container_g.append("g")
             .attr("transform", "translate(0, " + height / 2 + ")")
             .call(d3.axisBottom(xScale))
-            .append("text")
-            .attr("y", height - 450)
-            .attr("x", width - 375)
-            .attr("stroke", "black")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "20px")
-            .text("Time To Sell Out(Minutes)");
-
 
         container_g.append("g")
             .call(d3.axisLeft(yScale).tickFormat(function (d) {
                 return d;
             }).ticks())
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 20)
-            .attr("x", -100)
-            .attr("dy", "-4.1em")
-            .attr("stroke", "black")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "20px")
-            .text("Frequency");
 
         container_g.append("g")
             .call(d3.axisLeft(y).tickFormat(function (d) {
                 return d;
             }).ticks())
-            .attr("transform","translate("+((width/2)+30)+","+0+")")
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 20)
-            .attr("x", -100)
-            .attr("dy", "-4.1em")
-            .attr("stroke", "black")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "20px")
-            .text("Frequency");
-
+            .attr("transform", "translate(" + ((width / 2) + 30) + "," + 0 + ")")
 
         const computeBins = d3.bin()
             .value(function (d) { return d })
             .domain(xScale.domain())
             .thresholds(xScale.ticks(10));
 
-        const otherBin = d3.bin()
-            .value(function(d){return d})
-            .domain(x.domain())
-            .thresholds(x.ticks(10))
-        
-        const getOther = otherBin(distributions_thirtyeighty);
+        const getOther = computeBins(distributions_thirtyeighty);
         const bins = computeBins(distributions_thirtyseventy);
-
+        const getThird = computeBins(distributions_thirtyseventy_ti);
 
         const tooltip = d3.select("body")
             .append("div")
@@ -139,15 +108,14 @@ function main() {
                 .style("opacity", 0.5)
         }
 
-
         container_g.selectAll(".bar1")
-            .data(bins)
+            .data(getThird)
             .enter()
             .append("rect")
             .attr("class", "bar")
             .attr("x", function (d) { return xScale(d.x0) })
             .attr("y", function (d) { return yScale(d.length) })
-            .attr("width", function (d) { return xScale(d.x1) - xScale(d.x0)})
+            .attr("width", function (d) { return xScale(d.x1) - xScale(d.x0) })
             .style("fill", "green")
             .style("opacity", 0.5)
             .attr("height", function (d) { return height / 2 - yScale(d.length); })
@@ -180,8 +148,25 @@ function main() {
             })
             .on("mouseleave", mouseleave)
 
-
-
+        container_g.selectAll(".bar3")
+            .data(bins)
+            .enter()
+            .append("rect")
+            .attr("class", "bar3")
+            .attr("x", function (d) { return thirdX(d.x0) })
+            .attr("y", function (d) { return thirdY(d.length) })
+            .attr("width", function (d) { return thirdX(d.x1) - thirdX(d.x0) })
+            .style("fill", "green")
+            .style("opacity", 0.5)
+            .attr("height", function (d) { return height - thirdY(d.length); })
+            .on("mouseover", mouseover)
+            .on("mousemove", (Event, d) => {
+                tooltip
+                    .html("Frequency " + d.length)
+                    .style("left", (Event.x) / 2 - 100 + "px")
+                    .style("top", (Event.y) / 2 + "px")
+            })
+            .on("mouseleave", mouseleave)
     })
 }
 main();
