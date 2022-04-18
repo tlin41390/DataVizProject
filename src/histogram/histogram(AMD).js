6731d5781d69e2ef4c11e03d63cd1523b187d1b1
Terence Lin <56329045+tlin41390@users.noreply.github.com>
2022-04-18 13:43:31 -0700

    Update histogram(AMD).js

--- src/histogram/histogram(AMD).js
+++ src/histogram/histogram(AMD).js
@@ -25,7 +25,7 @@ function main() {
     const container_g = svg.append("g")
         .attr("transform", "translate(100,100)");
 
-    d3.csv("../../CSV/stock_and_time-AMD.csv").then(data => {
+    d3.csv("CSV/stock_and_time-AMD.csv").then(data => {
         xScale_thirtySeventyTi.domain([0, 60]);
         yScale_thirtySeventyTi.domain([0, 400]);
 
